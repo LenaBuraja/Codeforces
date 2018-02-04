959201ef063bd610a845ae53c8e849718f878845
Lena <lenaLatkovich@gmail.com>
2018-02-04 18:16:22 +0300

    912A

--- Python/912A.py
+++ Python/912A.py
@@ -2,7 +2,8 @@
 
 def main():
     k, s = list(map(int, input().split(' '))), list(map(int, input().split(' ')))
-    r: int = sum(map(lambda x, y: x*y, s, [2, 2, 3])) - sum(k)
+    t: list = [s[0] * 2 + s[1], s[1] + s[2] * 3]
+    r: int = sum(map(lambda x, y: y-x if (y-x) > 0 else 0, k, t))
     print(r)
     return True
 
